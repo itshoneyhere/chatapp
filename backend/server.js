975198776575dcd836c2987fcf44b4f615322997
Honey Raj <honey.kaustubh@gmail.com>
2022-03-10 13:42:39 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const { chats }= require("./data/data");
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send("Api is running");
+} );
+
+app.get('/api/chat',(req,res)=>{
+    res.send(chats);
+})
+
+app.get('/api/chat/:id',(req,res)=>{
+    console.log(req);
+});
+
+app.listen(5000,console.log("Server started on port 5000"));
+
